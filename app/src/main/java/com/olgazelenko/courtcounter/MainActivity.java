0756b98861a54d3c8d109726db3d212a8778d56b
zolga <ozgitdeploy@gmail.com>
2018-05-30 22:04:22 -0700

    support landscape mode

--- app/src/main/java/com/olgazelenko/courtcounter/MainActivity.java
+++ app/src/main/java/com/olgazelenko/courtcounter/MainActivity.java
@@ -9,20 +9,58 @@ public class MainActivity extends AppCompatActivity {
     final int ONE_POINT = 1;
     final int TWO_POINTS = 2;
     final int THREE_POINTS = 3;
+    final String STATE_TEAM_A = "TEAM_A";
+    final String STATE_TEAM_B = "TEAM_B";
 
     TextView scoreTeamAView;
     TextView scoreTeamBView;
 
-    public int scoreTeamA = 0;
-    public int scoreTeamB = 0;
+    public int scoreTeamA;
+    public int scoreTeamB;
 
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(STATE_TEAM_A, scoreTeamA);
+        outState.putInt(STATE_TEAM_B, scoreTeamB);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        scoreTeamA = savedInstanceState.getInt(STATE_TEAM_A);
+        scoreTeamB = savedInstanceState.getInt(STATE_TEAM_B);
+        setScore();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialStates();
+        if (savedInstanceState == null) {
+            scoreTeamA = 0;
+            scoreTeamB = 0;
+            setScore();
+        }
+    }
+
+    /**
+     * This method is called from the onCreate method and initial all the views.
+     */
+    private void initialStates() {
         scoreTeamAView = findViewById(R.id.team_a);
         scoreTeamBView = findViewById(R.id.team_b);
     }
+
+    /**
+     * This method displays current score of two teams.
+     */
+    private void setScore() {
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+
     /**
      * add three point score and displays updated score.
      */
@@ -72,7 +110,9 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamB(int score) {
         scoreTeamBView.setText(String.valueOf(score));
     }
-
+    /**
+     * Restore both teams score to zero.
+     */
     public void resetScore(View v)
     {
         scoreTeamA = 0;
